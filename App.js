76166d92833887597c2e72e287e6a7fcc3397ab4
Luis Angel Garcia Pineda <garciapinedaluisangel@gmail.com>
2021-03-03 17:56:01 -0600

    Modificación de app.js

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+} from "react-native";
 
-export default function App() {
+const Separator = () => <View style={styles.separator} />;
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{
+          uri:
+            "https://i.picsum.photos/id/1064/200/200.jpg?hmac=xUH-ovzKEHg51S8vchfOZNAOcHB6b1TI_HzthmqvcWU",
+        }}
+        style={styles.image}
+      />
+      <Text style={styles.title}>Hola, ser sin razón</Text>
+      <Text>Dime tu nombre</Text>
+      <Separator />
+
+      <Button title="Press me" onPress={() => console.log("Dimne tu nombre")} />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#292929",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: { fontSize: 25, color: "#fff" },
+  image: { width: 200, height: 200 },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: "#737373",
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
+
+export default App;
